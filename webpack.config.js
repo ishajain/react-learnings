bbd020e9073f4a85381a0df9adff91541a28b253
Isha Jain <isha.jain30@gmail.com>
2018-12-12 17:42:58 +1100

    Initial Setup Completed

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require("webpack");
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname,"dist");
+var SRC_DIR = path.resolve(__dirname,"src");
+
+var config = 
+{
+   entry: SRC_DIR + "/app/index.js",
+   output: { 
+       path: DIST_DIR + "/app",
+       filename:"bundle.js",
+       publicPath: "/app/"
+   },
+
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader"
+            }
+          },
+          
+        ]
+      },
+
+      resolve: {
+        extensions: ['*', '.js', '.jsx']
+      },
+    
+    
+
+   };          
+
+
+module.exports = config;
+
+
