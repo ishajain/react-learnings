541bd261b886e96b58145e867863d3a04bf8e948
Isha Jain <isha.jain30@gmail.com>
2018-12-13 14:49:21 +1100

    Code Splitting added to webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,31 @@ var SRC_DIR = path.resolve(__dirname,"src");
 
 var config = 
 {
-   entry: SRC_DIR + "/app/index.js",
+   entry: SRC_DIR + "/app/components/index.js",
    output: { 
        path: DIST_DIR + "/app",
-       filename:"bundle.js",
+       filename:"main.js",
+       chunkFilename: '[name].js',
        publicPath: "/app/"
    },
+
+   optimization: {
+    splitChunks: {
+        cacheGroups: {
+            default: false,
+            vendors: false,
+
+            // vendor chunk
+            vendor: {
+                // sync + async chunks
+                chunks: 'all',
+                name: 'vendor',
+                // import file path containing node_modules
+                test: /node_modules/
+            }
+        }
+    }
+},
 
     module: {
         rules: [
