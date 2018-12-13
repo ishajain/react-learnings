7bca0825942784aefe7b7e783e234e2f4a3eca46
Isha Jain <isha.jain30@gmail.com>
2018-12-13 15:30:34 +1100

    Added first react component

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,1 +1,17 @@
-console.log("It Works!!")
+import React from "react";
+import { render } from "React-DOM";
+
+class HomeComponent extends React.Component{
+
+    render(){
+       
+        return(
+            <div>
+                <h1>It works with react</h1>
+            </div>
+        );
+
+    }
+}
+
+render(<HomeComponent/>,document.getElementById("root"));
